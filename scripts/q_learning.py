35b854fb8f60e2e63730355726effa09ba1ed0ba
Yves Shum <yvesshum@uchicago.edu>
2021-04-26 14:08:19 -0500

    modified path imports for pre-generated matrices

--- scripts/q_learning.py
+++ scripts/q_learning.py
@@ -2,22 +2,32 @@
 
 import rospy
 import numpy as np
+import os
+
+# Path of directory on where this file is located
+path_prefix = os.path.dirname(__file__) + "/action_states/"
 
 class QLearning(object):
     def __init__(self):
         # Initialize this node
         rospy.init_node("q_learning")
 
-        # Fetch pre-built action matrix
-        self.action_matrix = np.loadtxt("../action_matrix.txt")
-
+        # Fetch pre-built action matrix. This is a 2d numpy array where row indexes
+        # correspond to the starting state and column indexes are the next states.
+        #
+        # A value of -1 indicates that it is not possible to get to the next state
+        # from the starting state. Values 0-9 correspond to what action is needed
+        # to go to the next state.
+        #
+        # e.g. self.action_matrix[0][12] = 5
+        self.action_matrix = np.loadtxt(path_prefix + "action_matrix.txt")
 
         # Fetch actions. These are the only 9 possible actions the system can take.
         # self.actions is an array of dictionaries where the row index corresponds
         # to the action number, and the value has the following form:
         # { dumbbell: "red", block: 1}
         colors = ["red", "green", "blue"]
-        self.actions = np.loadtxt("../actions.txt")
+        self.actions = np.loadtxt(path_prefix + "actions.txt")
         self.actions = list(map(
             lambda x: {"dumbbell": colors[int(x[0])], "block": int(x[1])},
             self.actions
@@ -31,11 +41,13 @@ class QLearning(object):
         # e.g. [0, 1, 2] indicates that the green dumbbell is at block 1, and blue at block 2.
         # A value of 0 corresponds to the origin. 1/2/3 corresponds to the block number.
         # Note: that not all states are possible to get to.
-        self.states = np.loadtxt("../states.txt")
+        self.states = np.loadtxt(path_prefix + "states.txt")
         self.states = list(map(lambda x: list(map(lambda y: int(y), x)), self.states))
 
     def save_q_matrix(self):
-        # TODO
+        # TODO: You'll want to save your q_matrix to a file once it is done to
+        # avoid retraining
         return
+
 if __name__ == "__main__":
     node = QLearning()
