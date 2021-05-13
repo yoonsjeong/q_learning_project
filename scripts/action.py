f45101b8f52f981e4aee44c77ab431e8db4a382d
Yoon Jeong <yoonsjeong@cs.uchicago.edu>
2021-05-13 02:09:46 -0500

    dante daunting dante

--- scripts/action.py
+++ scripts/action.py
@@ -7,10 +7,17 @@ import moveit_commander
 from q_learning_project.msg import RobotMoveDBToBlock
 import time
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
-# import keras_ocr
+import keras_ocr
+
+three = ["3", "s", "e", "5"]
+two = ["2"]
+one = ["1", "l", "1l"]
 class RobotAction(object):
     def __init__(self, robot_dumbbell, goal_block_num):
         self.robot_db = robot_dumbbell
+        if goal_block_num == 1: goal_block_num = one
+        elif goal_block_num == 2: goal_block_num = two
+        elif goal_block_num == 3: goal_block_num = three
         self.goal_block_num = goal_block_num
     def __str__(self):
         output_str = ("Robot action: move " + self.robot_db.upper() + " to block " + str(self.goal_block_num))
@@ -19,16 +26,16 @@ class RobotMovement(object):
     def __init__(self):
         # initialize this node
         rospy.init_node('turtlebot3_movement')
-        # # download pre-trained model
-        # pipeline = keras_ocr.pipeline.Pipeline()
+        # download pre-trained model
+        self.pipeline = keras_ocr.pipeline.Pipeline()
         # ROS subscribe to the topic publishing actions for the robot to take
         rospy.Subscriber("/q_learning/robot_action", RobotMoveDBToBlock, self.prepare_to_take_robot_action)
         # information about the robot action to take
-        self.robot_action_queue = [RobotAction("GREEN", 1)]
+        self.robot_action_queue = [RobotAction("BLUE", 1)]
         # set up ROS / cv bridge
         self.bridge = cv_bridge.CvBridge()
         # initalize the debugging window
-        cv2.namedWindow("window", 1)
+        # cv2.namedWindow("window", 1)
         # Vars to store current action goals
         self.selected_dumbbell = ""
         self.block_goal = 0
@@ -56,6 +63,9 @@ class RobotMovement(object):
         self.twist = Twist()
         self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=100)
 
+        # ocr
+        self.new_object = False
+        self.found_box = False
 
     def execute_phase_4(self):
         """ Resets the robot and pops the action from the queue.
@@ -90,42 +100,121 @@ class RobotMovement(object):
         move_arm.stop()
 
         self.phase = 4
-    def execute_phase_2(self, msg):
+    def execute_phase_2(self, image):
         """ Uses OCR image recognition to figure out what direction
             the numbered block is in
         """
         if self.phase != 2:
             return
         self.phase = -1
+
+        if self.found_box:
+            # Runs if we have detected the correct number on the box
+            print("heading to block")
+            # looking for black
+            lower = numpy.array([ 0, 0, 0])
+            upper = numpy.array([179, 255, 20])
+            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+            mask = cv2.inRange(hsv, lower, upper)
+            # we now erase all pixels that aren't of the selected color
+            h, w, d = image.shape
+            search_top = int(0)
+            search_bot = int(h)
+            mask[0:search_top, 0:w] = 0
+            mask[search_bot:h, 0:w] = 0
+            # using moments() function, determine the center of the colored pixels
+            M = cv2.moments(mask)
+            # if there are any colored pixels found
+            if M['m00'] > 0:
+                print("found colored pixels")
+                # determine the center of the colored pixels in the image
+                cx, cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
+                cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
+                k_p, err = .01, w/2 - cx
+                print(f"distance: {self.distance}")
+                if self.distance < 2:
+                    print("im close")
+                    self.twist.linear.x = 0
+                    self.twist.angular.z = 0
+                    self.cmd_vel_pub.publish(self.twist)
+                    self.found_box = False
+                    self.phase = 3
+                    print("my dad is so proud")
+                    return
+                else:
+                    print("im too far")
+                    self.twist.linear.x = 0.1
+                    self.twist.angular.z = k_p * err *.01
+                    self.cmd_vel_pub.publish(self.twist)
+                    self.phase = 2
+                    return
+
+        elif self.new_object:
+            # Runs if we have found a new object to perform ocr on
+            print("performing ocr on new object")
+            images = [image]
+            prediction_groups = self.pipeline.recognize(images)
+            time.sleep(30)
+            print(f"{prediction_groups}")
+            for num, box in prediction_groups[0]:
+                print(f"I detected a {num}")
+                if num in self.block_goal:
+                    print("FOUND THE BOX!")
+                    self.found_box = True
+                    self.new_object = False
+                    self.phase = 2
+                    return
+            self.new_object = False
+            print("did not find the box")
+            self.twist.angular.z =.1
+            self.cmd_vel_pub.publish(self.twist)
+            time.sleep(10)
+            self.twist.angular.z = 0
+            self.cmd_vel_pub.publish(self.twist)
+            print("done")
+            self.phase = 2
+
+        else:
+            # Runs if we are looking for a new object
+            print("looking for a new object")
+            # looking for black
+            lower = numpy.array([ 0, 0, 0])
+            upper = numpy.array([179, 255, 20])
+            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+            mask = cv2.inRange(hsv, lower, upper)
+            # we now erase all pixels that aren't of the selected color
+            h, w, d = image.shape
+            search_top = int(0)
+            search_bot = int(h)
+            mask[0:search_top, 0:w] = 0
+            mask[search_bot:h, 0:w] = 0
+            # using moments() function, determine the center of the colored pixels
+            M = cv2.moments(mask)
+            # if there are any colored pixels found
+            if M['m00'] > 0:
+                print("found colored pixels")
+                # determine the center of the colored pixels in the image
+                cx, cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
+                cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
+                k_p, err = .01, w/2 - cx
+                offset = 50
+                print(f"cx:{cx}, leftboundry:{w/2 - offset}, rightboundry: {w/2 + offset}")
+                if cx > w/2 - offset and cx < w/2 + offset:
+                    print("centered")
+                    self.new_object = True
+                    self.twist.angular.z = 0
+                    self.cmd_vel_pub.publish(self.twist)
+                else:
+                    print("adjusting")
+                    self.twist.angular.z = k_p * err * .05
+                    self.cmd_vel_pub.publish(self.twist)
+            else:
+                print("no colored pixels found")
+                self.twist.angular.z = .1
+                self.cmd_vel_pub.publish(self.twist)
+            self.phase = 2
+
         
-        self.twist.linear.x = -.2
-        self.cmd_vel_pub.publish(self.twist)
-        time.sleep(5)
-        self.twist.linear.x = 0
-        self.cmd_vel_pub.publish(self.twist)
-
-        self.phase = 3
-
-        # time.sleep(0.5)
-        # image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
-        # images = [image]
-        # prediction_groups = pipline.recognize(images)
-        # for num, box in prediction_groups[0]:
-        #     if num == self.block_goal:
-        #         # TODO: check that box is centered
-        #         print(box)
-        #         self.twist.linear.x = 0.1
-        #         # TODO: set appropriate value
-        #         while self.distance > 1:
-        #             timer.sleep(.5)
-        #         self.twist.linear.x = 0
-        #         self.phase = 3
-        #         return
-        # self.twist.angular.z = .1
-        # timer.sleep(.5)
-        # self.twist.angular.z = 0
-        # self.phase = 2
-        pass
 
     def execute_phase_1(self):
         """ Tells robot to pick up the dumbbell and 
@@ -160,6 +249,12 @@ class RobotMovement(object):
         move_arm.stop()
 
         print("Should have picked up dumbbell at this stage.")
+        self.twist.linear.x =-.2
+        self.cmd_vel_pub.publish(self.twist)
+        time.sleep(5)
+        self.twist.linear.x = 0
+        self.cmd_vel_pub.publish(self.twist)
+
         self.phase = 2
 
     def move_to_col(self, image, lower, upper):
@@ -184,7 +279,7 @@ class RobotMovement(object):
             cx, cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
             print(f"cy {cy} h {h}")
             cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
-            cv2.imshow("window", image)
+            # cv2.imshow("window", image)
             cv2.waitKey(3)
             k_p, err = .01, w/2 - cx
 
@@ -217,14 +312,14 @@ class RobotMovement(object):
         """
         # show the debugging window
         image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
-        cv2.imshow("window", image)
-        cv2.waitKey(3)
+        # cv2.imshow("window", image)
+        # cv2.waitKey(3)
         # route to phase
         if self.phase == 1:
             self.execute_phase_1()
             return
         elif self.phase == 2:
-            self.execute_phase_2(msg)
+            self.execute_phase_2(image)
             return
         elif self.phase == 3:
             self.execute_phase_3()
